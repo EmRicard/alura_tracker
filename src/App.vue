1574f06137ead4f6c1b301cbc2dca2b1ff1ffc74
Lucas Assis <lukas.ecomp@gmail.com>
2023-10-19 11:31:42 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <RegistroFormulario />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import RegistroFormulario from './components/RegistroFormulario.vue';
+
+export default defineComponent({
+    name: 'App',
+    components: { BarraLateral,
+      RegistroFormulario }
+});
+</script>
+
+<style>
+
+</style>
